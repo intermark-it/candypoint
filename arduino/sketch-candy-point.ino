4f2a66d8fa2395a335878439e05999c99e4b42f9
Miguel Angel Vila <mavila@grupointermark.com>
2018-04-29 15:58:15 +0200

    Added OLED display with SSD1306Ascii libraray

--- arduino/sketch-candy-point.ino
+++ arduino/sketch-candy-point.ino
@@ -11,7 +11,7 @@
 
   {"type":"status","code":"init"} // Inform the raspberry that the arduino is initializing
   {"type":"status","code":"ready"} // Inform the raspberry that the arduino is ready
-  {"type":"status","code":"waiting"} // Inform the raspberry that the arduino is waiting for a new RFID
+  {"type":"status","code":"waiting"} // Inform the raspberry that the arduino is waiting for a response
   {"type":"request","params":{ "rfid": "0x0000000000"}} // Send a request to raspberry to obtain info about a RFID
   {"type":"response","result":{ "rfid": "0x0000000000", "points": 100}} // Response received from raspberry with points associated to a RFID
   {"type":"response","result":{ "rfid": "0x0000000000", "pin": 1234}} // Response received from raspberry with pin associated to a RFID
@@ -27,6 +27,8 @@
 #include <SoftwareSerial.h>
 #include "RDM6300.h"
 #include <ArduinoJson.h>
+#include "SSD1306Ascii.h"
+#include "SSD1306AsciiAvrI2c.h"
 
 // Sensors pins
 #define BUTTON 8
@@ -36,31 +38,35 @@
 #define RFID_RX 2
 #define RFID_TX 3
 
+// RDIF reader
 SoftwareSerial rdm_serial(RFID_RX, RFID_TX);
 RDM6300<SoftwareSerial> rdm(&rdm_serial);
 
-// Minimum points required to unlock the door
-#define REQUIRED_POINTS 5
+// OLED I2C Address - 0x3C or 0x3D
+#define I2C_ADDRESS 0x3C
+// Define proper RST_PIN if required
+#define RST_PIN -1
+// OLED display
+SSD1306AsciiAvrI2c oled;
 
 // Struct that stores request info
 struct Request {
   String rfid;
-};
+} request;
 
 // Struct that stores response info
 struct Response {
   String rfid;
   int points;
   int pin;  
-};
+} response;
 
 // Variables
-String rfid = ""; // The last RFID identifier readed
-bool waiting = true; // Bool variable to control when read a new RFID
-bool buttonPressed = true; // Bool variable to control when button is pressed
+bool ready = true; // Bool variable to control when read a new RFID
 bool locked = true; // Bool variable to check is door is closed or open
 
 // Functions
+void initDisplay();
 void blinkLed(int pin, int n = 1);
 String readRFID();
 String parseData(unsigned long long data);
@@ -69,11 +75,18 @@ void lock();
 void sendStatus(String status);
 void sendRequest(struct Request *request);
 struct Response getResponse();
+void waitForLock();
+void showPin(String pin);
+void showPointsAndUnlock(String points);
+void displayText(String header, String body);
+void displayScrollText(String text, bool endDelay);
 
 // Arduino functions ---------------------------------------------------------
 void setup() {
   Serial.begin(9600);
   while (!Serial) continue;
+  // Initialize OLED display
+  initDisplay();
   // Send init status
   sendStatus("init");
   // Init leds and button
@@ -82,51 +95,52 @@ void setup() {
   pinMode(BUTTON, INPUT);
   // Lock the door (servo)
   lock();
-  // Send ready status
-  sendStatus("ready");
 }
 
 void loop() {
-  if (waiting) { // Wait for new rfid
-    // Send waiting status
-    sendStatus("waiting");
+  if (ready) { // Wait for a new RFID
+    // Send ready status
+    sendStatus("ready");
     // Read RFID from the card reader
-    rfid = readRFID();
+    request.rfid = readRFID();
     // Send RFID obtained to Raspberry
-    Request request;
-    request.rfid = rfid;
     sendRequest(&request);
-    // Change waiting state to receive data from Raspberry
-    waiting = false;
+    // Change ready state to receive data from Raspberry
+    ready = false;
   } else { // Wait and check response from Raspberry
-    if (locked) { // Door is closed
-      while (Serial.available() == 0) {}
-      Response response = getResponse();
-      // Check that result rfid is same that last readed
-      if (response.rfid == rfid) {
-        if (response.pin > 0) { // Raspberry returns a pin code, it corresponding to new user
-          // TODO send pin code to display
-          waiting = true;
-        } else {
-          if (response.points > REQUIRED_POINTS) { // Raspberry response is ok, unlock the door
-            unlock();
-          } else
-            waiting = true;
+    if (locked) {
+      // Send waiting status
+      sendStatus("waiting");
+      // Get response from Raspberry
+      response = getResponse(&request);
+      // Check that response is valid (RFID is not empty)
+      if (response.rfid != "") {
+        if (response.pin > 0) { // Raspberry returns a pin code, it corresponding to a new user
+          showPin(response.pin);
+        } else { // Raspberry return points, check if sufficient to unlock the door
+          showPointsAndUnlock(response.points);
+          return;
         }
-      } else
-        waiting = true;
-    } else { // Door is open
-      // Check if door is open and button pressed, then lock de door and prepare for a new rfid
-      buttonPressed = (digitalRead(BUTTON) == LOW);
-      if (buttonPressed) {
-        lock();
-        waiting = true;
-      }      
+      }
+      // Change ready state to read a new RFID from Arduino
+      ready = true;
+    } else {
+      // Check if button is pressed (close the door and change to ready status)
+      waitForLock();
     }
   }
 }
 
-// Internal functions ---------------------------------------------------------
+// Sketch functions ---------------------------------------------------------
+
+// Initialize OLED display with the I2C addr 0x3C (for the 128x64)
+void initDisplay() {
+  #if RST_PIN >= 0
+    oled.begin(&Adafruit128x64, I2C_ADDRESS, RST_PIN);
+  #else // RST_PIN >= 0
+    oled.begin(&Adafruit128x64, I2C_ADDRESS);
+  #endif // RST_PIN >= 0
+}
 
 // Blink led "n" times
 void blinkLed(int pin, int n = 1) {
@@ -141,11 +155,11 @@ void blinkLed(int pin, int n = 1) {
 // Read RFID from the card reader
 String readRFID() {
   String rfid = "";
-  // The "read" method loop until a new rfid is ready
+  // The "read" method loop until a new RFID is ready
   unsigned long previousMillis = millis();
   do {
     rfid = parseData(rdm.read());
-  } while ((millis() - previousMillis) <= 2000);
+  } while ((millis() - previousMillis) <= 1000);
   return rfid;
 }
 
@@ -178,6 +192,7 @@ void unlock() {
   blinkLed(GREEN_LED, 3);
   digitalWrite(GREEN_LED, HIGH);
   servo.detach();
+  ready = false;
   locked = false;
 }
 
@@ -190,6 +205,7 @@ void lock() {
   blinkLed(RED_LED, 3);
   digitalWrite(RED_LED, HIGH);
   servo.detach();
+  ready = true;
   locked = true;
 }
 
@@ -203,6 +219,9 @@ void sendStatus(String status) {
   root.printTo(jsonStatus);
   Serial.println(jsonStatus);    
   delay(100);
+  // Capitalize status to show in display
+  status.setCharAt(0, toupper(status.charAt(0)));
+  displayText("CANDY POINT", status);
 }
 
 // Send request to Raspberry
@@ -219,18 +238,93 @@ void sendRequest(struct Request *request) {
 }
 
 // Get response from Raspberry
-struct Response getResponse() {
+struct Response getResponse(struct Request *request) {
+  // Wait until data available form Raspberry
+  while (Serial.available() == 0) {}
+  // Read and parse JSON response
   Response response;
+  response.rfid = "";
+  response.points = 0;
+  response.pin = 0;
   DynamicJsonBuffer jsonBuffer;
   JsonObject& root = jsonBuffer.parseObject(Serial);
   // It's a json object
   if (root.success()) {
     if (root["type"] == "response") {
-      response.rfid = root["result"]["rfid"].as<String>();
-      response.points = root["result"]["points"].as<int>();
-      response.pin = root["result"]["pin"].as<int>();
+      if (request->rfid == root["result"]["rfid"].as<String>()) {
+        response.rfid = root["result"]["rfid"].as<String>();
+        response.points = root["result"]["points"].as<int>();
+        response.pin = root["result"]["pin"].as<int>();
+      }
     }
   }
   return response;
+}
+
+// If button is pressed, lock the door, and change status to ready for read a new RFID
+void waitForLock() {
+  static bool buttonPressed = true;
+  buttonPressed = (digitalRead(BUTTON) == LOW);
+  if (buttonPressed) {
+    lock();
+  }      
+}
+
+// Display pin code in OLED display
+void showPin(int pin) {
+  displayScrollText("Sorry, you are not registered, please register using the following PIN...", false);
+  displayText("PIN", String(pin));
+}
+
+// Display remaining points in OLED display and unlock the door if has sufficient points
+void showPointsAndUnlock(int points) {
+  if (points > -1) { // User has sufficient points
+    unlock();
+    displayText("POINTS", String(points));
+    displayScrollText("Take your candy and...  ENJOY!!!", true);
+  } else { // User has insufficient points
+    lock();
+    displayScrollText("You don't have enough points...  SORRY!!!", true);
+  }
+}
+
+// Display a message with header and body in OLED display at big font size
+void displayText(String header, String body) {
+  oled.clear();
+  oled.setFont(Adafruit5x7);
+  oled.set1X();
+  oled.setCursor(oled.displayWidth()/2 - header.length()*6/2, 0);
+  oled.println(header);
+  oled.println();
+  oled.set2X();
+  oled.setCursor(oled.displayWidth()/2 - body.length()*12/2, oled.displayHeight()/2);
+  oled.println(body);
+  delay(3000);
+}
+
+// Display scrolling text in OLED display
+void displayScrollText(String text, bool endDelay) {
+  const int width = 10; // Width of the marquee display (in characters)
+  const char termination = '$'; // Special character ($) to determine the end of text
+  oled.clear();  
+  oled.set2X();
+  String message = text + termination;
+  // Loop once through the string
+  for (int offset = 0; offset < message.length() && message.charAt((offset + width-1) % message.length()) != termination; offset++) {
+    oled.clear();  
+    // Construct the string to display for this iteration
+    String t = "";
+    for (int i = 0; i < width; i++)
+      t += message.charAt((offset + i) % message.length());
+    // Print the string for this iteration
+    oled.println();
+    oled.println(t);
+    if (offset == 0)
+      delay(750); // Significant delay before start scrolling
+    else
+      delay(250); // Short delay so the text doesn't move too fast
+  }
+  if (endDelay) // After scroll, delay 3 seconds
+    delay(3000);
 }
 
